6f5607e1a9fcda068afcd3b362281caac877270e
Ali-Dahaj <ali.dahaj@hcbioscience.com>
2023-02-27 11:43:51 -0500

    updating the streamlit_cars.py

--- streamlit_cars.py
+++ streamlit_cars.py
@@ -6,28 +6,48 @@ Created on Sun Feb 26 13:28:19 2023
 """
 import streamlit as st
 import pandas as pd
+#%%
+file_path = 'C:/Users/AliDahaj/Documents/python_scripts/cars.xlsx'
+FILE_PATH = 'C:/Users/AliDahaj/Documents/python_scripts/cars.xlsx'
 
-def add_car_to_excel(color, size, model):
-    # Read the Excel file
-    df = pd.read_excel('cars.xlsx')
-    
-    # Check if there is already a car with the same properties
-    if ((df['Color'] == color) & (df['Size'] == size) & (df['Model'] == model)).any():
-        st.warning("A car with these properties already exists in the Excel file.")
-    else:
-        # Add the car to the Excel file
-        new_car = {'Color': color, 'Size': size, 'Model': model}
-        df = df.append(new_car, ignore_index=True)
-        df.to_excel('cars.xlsx', index=False)
-        st.success("Car added to the Excel file!")
+def is_car_duplicate(color, model, year):
+    df = pd.read_excel(FILE_PATH, index_col= 'car num')
+    is_duplicate = df[(df["Color"].str.lower() == color.lower()) &
+                      (df["Model"].str.lower() == model.lower()) &
+                      (df["Year"] == year)].shape[0] > 0
+    return is_duplicate
 
-# Set up the Streamlit app
-st.title("Car Adder")
+# Define function to add a new car to the Excel file
+def add_car(color, model, year):
+    df = pd.read_excel(FILE_PATH, index_col = 'car num')
+    max_car_number = df.shape[0]
+    new_car_number = max_car_number + 1
+    df.loc[new_car_number] = [color, model, year]
+    df.to_excel(FILE_PATH, index = True)
+    st.success(f"New car with car number {new_car_number} has been added to the file.")
 
-color = st.text_input("Enter the color of the car:")
-size = st.text_input("Enter the size of the car:")
-model = st.text_input("Enter the model of the car:")
+# Define the Streamlit app
+def main():
+    # Set page title
+    st.set_page_config(page_title="Add Car")
 
-if st.button("Add Car"):
-    add_car_to_excel(color, size, model)
+    # Add page title
+    st.title("Add Car")
+
+    # Add form for user to input car information
+    color = st.selectbox('Select car color:', ['Red', 'Blue', 'Green', 'Yellow'])
+    model = st.text_input("Enter the car model", value="SUV")
+    year = st.number_input("Enter the car year", value=2021, min_value=1900, max_value=2100)
+
+    # Add button to submit the form
+    if st.button("Add"):
+        # Check if the car already exists in the Excel file
+        if is_car_duplicate(color, model, year):
+            st.warning("This car already exists in the file.")
+        else:
+            add_car(color, model, year)
+    st.dataframe(pd.read_excel(FILE_PATH, index_col = 'car num'), 300, 300)
+
+if __name__ == "__main__":
+    main()
 
